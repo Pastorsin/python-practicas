b62ec8b523374e04afebd48e6daed709670cac29
Andres Milla <andressmilla@gmail.com>
2018-03-29 23:58:39 -0300

    ej 4 y 5 agregados

---
+++ Practica1/ej5.py
@@ -0,0 +1,4 @@
+frase = "Si trabajás mucho CON computadoras, eventualmente encontrarás que te gustaría automatizar alguna tarea. Por ejemplo, podrías desear realizar una búsqueda y reemplazo en un gran número DE archivos de texto, o renombrar y reorganizar un montón de archivos con fotos de una manera compleja. Tal vez quieras escribir alguna pequeña base de datos personalizada, o una aplicación especializada con interfaz gráfica, o UN juego simple."
+lista = frase.lower().split(' ')
+lista = list(set(lista))
+print(lista)
