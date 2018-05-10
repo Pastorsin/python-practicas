0fcec2e4c82abfe76c4228f521766a404f7c7b0d
Andres Milla <andressmilla@gmail.com>
2018-05-10 07:29:19 -0300

    Practica 3 terminada.

--- Practica3/ej06bis.py
+++ Practica3/ej06bis.py
@@ -7,18 +7,14 @@ import csv
 def exportar_csv_a_json(nombre):
     archivo_json = open(nombre + ".json", "w")
     archivo_csv = open(nombre + ".csv", "r")
-    lista_csv = list(csv.reader(archivo_csv))
-    encabezado_csv = lista_csv[0]
-    lista_json = []
 
-    for valor_csv in lista_csv[1:]:
-        dicc_json = {}
-        for i in range(len(encabezado_csv)):
-            #print("{0:50} : {1:90}".format(encabezado_csv[i],valor_csv[i]))
-            dicc_json[encabezado_csv[i].strip()] = valor_csv[i].strip()
-        lista_json.append(dicc_json)
-
+    contenido_csv = csv.reader(archivo_csv)
+    encabezado_csv = next(contenido_csv)
+    lista_json = list()
+    for fila in contenido_csv:
+        lista_json.append(dict(zip(encabezado_csv, list(map(lambda x: x.strip() if x else '0' ,fila)))))
     json.dump(lista_json, archivo_json, ensure_ascii=False)
+
 
     archivo_json.close()
     archivo_csv.close()
