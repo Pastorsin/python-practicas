ffa4f882edcaead8bd43265f028fdc72921e269a
Andres Milla <andressmilla@gmail.com>
2018-06-06 14:27:28 -0300

    Codigo simplificado

--- Practica4/ej07.py
+++ Practica4/ej07.py
@@ -31,44 +31,27 @@ class Jugador():
         return self.__lista_puntajes
 
     def nivelMaximo(self):
-        nivel_maximo = -1
-        for puntaje in self.getListaPuntajes():
-            nivel_maximo = max(nivel_maximo, puntaje.getNivel())
-        return nivel_maximo
+        return max(self.getListaPuntajes(), key= lambda x: x.getNivel()).getNivel()
 
     def cantidadTotalPuntajes(self):
-        suma = 0
-        for puntaje in self.getListaPuntajes():
-            suma += puntaje.getPuntos()
-        return suma
+        return sum(x.getPuntos() for x in self.getListaPuntajes())
 
     def puntajeMaximo(self, nivel):
-        puntaje_maximo = -1
-        for puntaje in self.getListaPuntajes():
-            if puntaje.getNivel() == nivel:
-                puntaje_maximo = max(puntaje_maximo, puntaje.getPuntos())
-        return puntaje_maximo
+        lista_niveles = list(filter(lambda x: x.getNivel() == nivel,self.getListaPuntajes()))
+        return max(lista_niveles, key= lambda x: x.getPuntos()).getPuntos()
 
     def menorTiempo(self, nivel):
-        tiempo_minimo = sys.maxsize
-        for puntaje in self.getListaPuntajes():
-            if puntaje.getNivel() == nivel:
-                tiempo_minimo = min(tiempo_minimo, puntaje.getTiempo())
-        return tiempo_minimo
+        lista_niveles = list(filter(lambda x: x.getNivel() == nivel,self.getListaPuntajes()))
+        return min(lista_niveles, key= lambda x: x.getTiempo()).getTiempo()
 
 
 if __name__ == "__main__":
     puntaje1 = Puntaje(1, 20, 30)
     puntaje2 = Puntaje(5, 50, 60)
     puntaje3 = Puntaje(2, 90, 20)
-    jugador = Jugador("Andres", [puntaje1, puntaje2, puntaje3])
+    puntaje4 = Puntaje(2, 9000, 20)
+    jugador = Jugador("Andres", [puntaje1, puntaje2, puntaje3, puntaje4])
     print(jugador.nivelMaximo())
     print(jugador.cantidadTotalPuntajes())
     print(jugador.puntajeMaximo(2))
     print(jugador.menorTiempo(1))
-
-
-# env
-# git
-# gitignore env
-# pip install -r requirements
